f86ee96f0e865f1f1a86020206bfcce5bc6e122e
gnismas <82707890+gnismas@users.noreply.github.com>
2021-04-17 15:31:31 +0200

    Add files via upload

---
+++ src/Students.java
@@ -0,0 +1,12 @@
+public class Students {
+  public Students(String name, int[] grade) {
+    this.name = name;
+    this.grade = grade;
+  }
+
+  private String name;
+  private int[] grade;
+  Students student1 = new Students("Klaes",new int[] {7});
+
+}
+
